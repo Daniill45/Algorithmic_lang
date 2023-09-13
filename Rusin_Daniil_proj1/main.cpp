338b7afcdc1fec470dc672598ccf58f59620eb83
danru00@mail.ru <danru00@mail.ru>
2023-09-13 11:10:57 +0300

    cpp file with 'hello world' and output

---
+++ Rusin_Daniil_proj1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string x;
+    cout << "Hello World!"<<endl;
+    cin>>x;
+    cout << "your input: ";
+    cout << x<<endl;
+    return 0;
+}
