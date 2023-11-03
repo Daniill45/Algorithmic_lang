ff4be845713c255dc7699d7af5be88bdcdbc7425
Daniill45 <92102406+Daniill45@users.noreply.github.com>
2023-11-03 11:16:05 +0300

    only one pipe and station

--- FIRST_Laba_Rusin/main.cpp
+++ FIRST_Laba_Rusin/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
 const int empty_strings = 1000;
+const double inf = 1e123;
 
 struct Pipeline
 {
-    string name = "NO_NAME";
+    string name = "";
     double length = 0;
     double diametr = 0;
     bool in_repair = false;
@@ -17,220 +17,161 @@ struct Pipeline
 
 struct Compressor
 {
-    string name = "NO_NAME";
+    string name = "";
     int manufact = 0;
     int manufact_in_work = 0;
     double efficiency = 0;
 };
-void pipe_validation(double result[])
+template <typename T1, typename T2, typename T3>
+void is_valid(T1 &varible, T2 a, T3 b)
 {
-    double lenght = -1;
-    double diametr = -1;
-    bool in_repair = true;
-    cout << "Введите длину:" << endl;
-    while (!(cin >> lenght) || (lenght <= 0) || (cin.peek() != '\n'))
-    {
-        cout << "Ошибка: Введите корректное значение длины: ";
-        cin.clear();
-        cin.ignore(empty_strings, '\n');
-    }
-    cout << "Введите диаметр:" << endl;
-    while (!(cin >> diametr) || (diametr <= 0) || (cin.peek() != '\n'))
-    {
-        cout << "Ошибка: Введите корректное значение диаметра: ";
-        cin.clear();
-        cin.ignore(empty_strings, '\n');
-    }
-    cout << "Введите 1 если труба в ремонте, иначе 0:" << endl;
-    while (!(cin >> in_repair) || (cin.peek() != '\n'))
+
+    while (!(cin >> varible) || (varible < a) || (varible > b) || (cin.peek() != '\n'))
     {
         cout << "Ошибка: Введите корректное значение: ";
         cin.clear();
         cin.ignore(empty_strings, '\n');
     }
-    result[0] = lenght;
-    result[1] = diametr;
-    result[2] = in_repair;
 }
-void cs_validation(double result2[])
+
+void create_pipe(Pipeline &pipe)
 {
+    string name;
+    double lenght = -1;
+    double diametr = -1;
+    bool in_repair = 1;
+    cout << "Введите название трубы:" << endl;
+    cin >> ws;
+    getline(cin, name);
+    cout << "Введите длину:" << endl;
+    is_valid(lenght, 0.0, double(inf));
+    cout << "Введите диаметр:" << endl;
+    is_valid(diametr, 0.0, double(inf));
+    cout << "Введите 1 если труба в ремонте, иначе 0:" << endl;
+    is_valid(in_repair, bool(0), bool(1));
+    pipe.name = name;
+    pipe.length = lenght;
+    pipe.diametr = diametr;
+    pipe.in_repair = in_repair;
+}
+void create_CS(Compressor &cs)
+{
+    string name;
     int manufact = -1;
     int manufact_in_work = -1;
     double effic = -1;
-    cout << "Введите количество цехов:" << endl;
-    while (!(cin >> manufact) || (manufact <= 0) || (cin.peek() != '\n'))
-    {
-        cout << "Ошибка: Введите корректное количество цехов: ";
-        cin.clear();
-        cin.ignore(empty_strings, '\n');
-    }
-    cout << "Введите количество цехов в работе:" << endl;
-    while (!(cin >> manufact_in_work) || (cin.peek() != '\n') || (manufact_in_work < 0) || (manufact_in_work > manufact))
-    {
-        cout << "Ошибка: Введите корректное количество цехов в работе: ";
-        cin.clear();
-        cin.ignore(empty_strings, '\n');
-    }
-    cout << "Введите эффективность:" << endl;
-    while (!(cin >> effic) || (effic < 0) || (effic > 100) || (cin.peek() != '\n'))
-    {
-        cout << "Ошибка: Введите корректное значение эффективности: ";
-        cin.clear();
-        cin.ignore(empty_strings, '\n');
-    }
-    result2[0] = manufact;
-    result2[1] = manufact_in_work;
-    result2[2] = effic;
-}
-void create_pipe(vector<Pipeline> &pipes)
-{
-    double results[3];
-    Pipeline tmp_pipe;
-    cout << "Введите название трубы:" << endl;
-    cin >> tmp_pipe.name;
-    pipe_validation(results);
-    tmp_pipe.length = results[0];
-    tmp_pipe.diametr = results[1];
-    tmp_pipe.in_repair = results[2];
-
-    pipes.push_back(tmp_pipe);
-}
-void create_CS(vector<Compressor> &stations)
-{
-    double results[3];
-    Compressor cs;
     cout << "Введите название кс:" << endl;
-    cin >> cs.name;
-    cs_validation(results);
-    cs.manufact = results[0];
-    cs.manufact_in_work = results[1];
-    cs.efficiency = results[2];
-
-    stations.push_back(cs);
+    cin >> ws;
+    getline(cin, name);
+    cs.name = name;
+    cout << "Введите количество цехов:" << endl;
+    is_valid(manufact, 0, inf);
+    cout << "Введите количество цехов в работе:" << endl;
+    is_valid(manufact_in_work, 0, manufact);
+    cout << "Введите эффективность:" << endl;
+    is_valid(effic, 0.0, 100.0);
+    cs.manufact = manufact;
+    cs.manufact_in_work = manufact_in_work;
+    cs.efficiency = effic;
 }
-void edit_pipe(vector<Pipeline> &pipes)
+
+void edit_pipe(Pipeline &pipe)
 {
-    string input_name;
-    cout << "Введите название трубы:" << endl;
-    cin >> input_name;
     bool in_repair;
-    int flag = 0;
-    for (Pipeline &i : pipes)
-    {
-        if (i.name == input_name)
-        {
-            flag = 1;
-            cout << "Введите 1 если труба в ремонте, иначе 0:" << endl;
-            while (!(cin >> in_repair) || (cin.peek() != '\n'))
-            {
-                cout << "Ошибка: Введите корректное значение: ";
-                cin.clear();
-                cin.ignore(empty_strings, '\n');
-            }
-            i.in_repair = in_repair;
-            break;
-        }
-    }
-    if (flag == 0)
+    if (pipe.name.empty())
     {
         cout << "Труба не нашлась..." << endl;
     }
+    else
+    {
+        cout << "Введите 1 если труба в ремонте, иначе 0" << endl;
+        is_valid(in_repair, 0, 1);
+        pipe.in_repair = in_repair;
+    }
 }
-void edit_cs(vector<Compressor> &stations)
+void edit_cs(Compressor &station)
 {
-    string input_name;
-    cout << "Введите название КС:" << endl;
-    cin >> input_name;
-    int flag = 0;
+
     int manufact_in_work;
-    for (Compressor &i : stations)
+
+    if (station.name.empty())
     {
-        if (i.name == input_name)
-        {
-            flag = 1;
-            cout << "Введите количество цехов в работе:" << endl;
-            while (!(cin >> manufact_in_work) || (cin.peek() != '\n') || (manufact_in_work < 0) || (manufact_in_work > i.manufact))
-            {
-                cout << "Ошибка: Введите корректное значение: ";
-                cin.clear();
-                cin.ignore(empty_strings, '\n');
-            }
-            i.manufact_in_work = manufact_in_work;
-            break;
-        }
+        cout << "Компрессорная станция не нашлась" << endl;
     }
-    if (flag == 0)
+    else
     {
-        cout << "НПС не нашлась" << endl;
+        cout << "Введите количество цехов в работе:" << endl;
+        is_valid(manufact_in_work, 0, station.manufact);
+        station.manufact_in_work = manufact_in_work;
     }
 }
-void show_all(vector<Compressor> &stations, vector<Pipeline> &pipes)
+void show_pipe(Pipeline &pipe)
 {
-    if (!stations.empty())
-    {
-        cout << "КС:" << endl;
-    }
-    for (Compressor i : stations)
+    if (!pipe.name.empty())
     {
         cout << endl;
-        cout << "Название:" << i.name << endl;
-        cout << "Количество цехов:" << i.manufact << endl;
-        cout << "Количество цехов в работе:" << i.manufact_in_work << endl;
-        cout << "Эффективность:" << i.efficiency << endl;
-    }
-    if (!pipes.empty())
-    {
-        cout << endl;
-        cout << "Трубопроводы:" << endl;
-    }
-    for (Pipeline i : pipes)
-    {
-        cout << endl;
-        cout << "Название:" << i.name << endl;
-        cout << "Длина:" << i.length << endl;
-        cout << "Диаметр:" << i.diametr << endl;
-        cout << "На ремонте:" << (i.in_repair ? "Да" : "Нет") << endl;
-    }
-    if (pipes.empty() && stations.empty())
-    {
-        cout << "Ничего не нашлось..." << endl;
+        cout << "Трубопровод:" << endl;
+        cout << "Название:" << pipe.name << endl;
+        cout << "Длина:" << pipe.length << endl;
+        cout << "Диаметр:" << pipe.diametr << endl;
+        cout << "На ремонте:" << (pipe.in_repair ? "Да" : "Нет") << endl;
     }
 }
-void save_data(const vector<Pipeline> &pipes, const vector<Compressor> &stations)
+void show_station(Compressor &station)
 {
-    string file_name;
-    cout << "Введите имя файла на сохранение: ";
-    cin >> file_name;
-    ofstream f(file_name);
+    if (!station.name.empty())
+    {
+        cout << endl;
+        cout << "Компрессорная станция:" << endl;
+        cout << "Название:" << station.name << endl;
+        cout << "Количество цехов:" << station.manufact << endl;
+        cout << "Количество цехов в работе:" << station.manufact_in_work << endl;
+        cout << "Эффективность:" << station.efficiency << endl;
+    }
+}
+void save_pipe(Pipeline &pipe, string file_name)
+{
+
+    ofstream f(file_name, ios::app);
 
     if (!f)
     {
         cout << "Ошибка открытия файла для сохранения: " << file_name << endl;
         return;
     }
-
-    for (const Pipeline &i : pipes)
+    if (!pipe.name.empty())
     {
         f << "Трубопровод:\n";
-        f << i.name << "\n";
-        f << i.length << "\n";
-        f << i.diametr << "\n";
-        f << i.in_repair << "\n";
+        f << pipe.name << "\n";
+        f << pipe.length << "\n";
+        f << pipe.diametr << "\n";
+        f << pipe.in_repair << "\n";
     }
-    for (const Compressor &j : stations)
+    f.close();
+}
+void save_compressor(Compressor &station, string file_name)
+{
+    ofstream f(file_name, ios::app);
+
+    if (!f)
+    {
+        cout << "Ошибка открытия файла для сохранения: " << file_name << endl;
+        return;
+    }
+    if (!station.name.empty())
     {
         f << "КС:\n";
-        f << j.name << "\n";
-        f << j.manufact << "\n";
-        f << j.manufact_in_work << "\n";
-        f << j.efficiency << "\n";
+        f << station.name << "\n";
+        f << station.manufact << "\n";
+        f << station.manufact_in_work << "\n";
+        f << station.efficiency << "\n";
     }
 
     f.close();
     cout << "Данные сохранены в файл: " << file_name << endl;
 }
 
-void load_data(vector<Pipeline> &pipes, vector<Compressor> &stations)
+void load_data(Pipeline &pipe, Compressor &station)
 {
     string file_name;
     cout << "Введите имя файла на загрузку: ";
@@ -241,24 +182,18 @@ void load_data(vector<Pipeline> &pipes, vector<Compressor> &stations)
         cout << "Ошибка открытия файла на загрузку: " << file_name << endl;
         return;
     }
-    pipes.clear();
-    stations.clear();
-    string line;
 
+    string line;
     while (getline(f, line))
     {
         if (line == "Трубопровод:")
         {
-            Pipeline pipe;
             f >> pipe.name >> pipe.length >> pipe.diametr >> pipe.in_repair;
-            pipes.push_back(pipe);
             f.ignore(empty_strings, '\n');
         }
         else if (line == "КС:")
         {
-            Compressor cs;
-            f >> cs.name >> cs.manufact >> cs.manufact_in_work >> cs.efficiency;
-            stations.push_back(cs);
+            f >> station.name >> station.manufact >> station.manufact_in_work >> station.efficiency;
             f.ignore(empty_strings, '\n');
         }
     }
@@ -266,18 +201,6 @@ void load_data(vector<Pipeline> &pipes, vector<Compressor> &stations)
     cout << "Данные загружены из файла: " << file_name << endl;
 }
 
-int check_valid_int()
-{
-    int MODE;
-    while (!(cin >> MODE) || (cin.peek() != '\n') || (MODE > 7) || (MODE < 0))
-    {
-        cout << "Ошибка: Введите корректное значение:";
-        cin.clear();
-        cin.ignore(empty_strings, '\n');
-    }
-
-    return MODE;
-}
 void Menu()
 {
     cout << "1-Добавить трубу" << endl
@@ -291,49 +214,59 @@ void Menu()
 }
 int main()
 {
-    vector<Pipeline> pipes;
-    vector<Compressor> stations;
+    int MODE = 0;
+    Pipeline pipe;
+    Compressor station;
     while (1)
     {
         Menu();
-        int MODE = check_valid_int();
+        is_valid(MODE, 0, 7);
         if (MODE != 0)
         {
             switch (MODE)
             {
             case 1:
             {
-                create_pipe(pipes);
+                create_pipe(pipe);
                 break;
             }
             case 2:
             {
-                create_CS(stations);
+                create_CS(station);
                 break;
             }
             case 3:
             {
-                show_all(stations, pipes);
+                show_pipe(pipe);
+                show_station(station);
+                if ((pipe.name.empty()) && (station.name.empty()))
+                {
+                    cout << "Ничего не нашлось..." << endl;
+                }
                 break;
             }
             case 4:
             {
-                edit_pipe(pipes);
+                edit_pipe(pipe);
                 break;
             }
             case 5:
             {
-                edit_cs(stations);
+                edit_cs(station);
                 break;
             }
             case 6:
             {
-                save_data(pipes, stations);
+                string file_name;
+                cout << "Введите имя файла на сохранение: ";
+                cin >> file_name;
+                save_compressor(station, file_name);
+                save_pipe(pipe, file_name);
                 break;
             }
             case 7:
             {
-                load_data(pipes, stations);
+                load_data(pipe, station);
                 break;
             }
             }
